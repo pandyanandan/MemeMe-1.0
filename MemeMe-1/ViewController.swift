d691f7319fe88fe5faf854592bcce791a0149701
Nandan Pandya <pandyanandan007@gmail.com>
2017-02-08 22:50:25 +0530

    ImagePicker added

--- MemeMe-1/ViewController.swift
+++ MemeMe-1/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var UIImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func PickAnImage(_ sender: Any) {
+        print("Pick Image")
+        let pickerController = UIImagePickerController()
+        present(pickerController, animated: true, completion: nil)
+
+    }
 
 }
-
